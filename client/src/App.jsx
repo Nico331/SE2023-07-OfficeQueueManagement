4a1141b33dae5897a0cea5f1106e881f3ec7129d
filippobertolotti0 <filippobertolotti0@gmail.com>
2023-10-25 22:38:46 +0200

    added MainBoard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,59 @@
 import './App.css';
 import TicketList from "./Components/ShowTickets";
-import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import MainBoard from './Components/mainBoard/ShowMainBoard';
 import CounterScreen from './Components/officer/counterScreen'
 import CustomerScreen from './Components/customer/customerScreen'
+import axios from "axios";
 
 function App() {
-  return (
-      //<TicketList/>
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/mainBoard' element={<MainBoard/>}/>
-            <Route path='/counter/:id' element={<CounterScreen/>}/>
-            <Route path='/customer' element={<CustomerScreen/>}/>
-          </Routes>
-        </BrowserRouter>
-      </>
-  );
+    const [counters, setCounters] = useState([]);
+    const [firstLoad, setFirstLoad] = useState(false);
+    const [refresh, setRefresh] = useState(false);
+    let list = [];
+
+    useEffect(()=>{
+        if(!firstLoad){
+            setFirstLoad(!firstLoad);
+        }
+        else{
+            axios.get("http://localhost:8080/API/getcounters")
+                .then(async result => {
+                    for (const element of result.data) {
+                        await axios.get(`http://localhost:8080/API/ticket/counter/${element.id}`)
+                            .then(result => {
+                                list.push({counterID: element.id, nextCustomer: result.data.id, service: result.data.serviceTypeId})
+                                console.log(result.data);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            });
+                    }
+                })
+                .then(() => {
+                    setCounters(list);
+                    list = [];
+                })
+                .catch(err => {
+                    console.log('error in catch', err);
+                });
+        }
+    }, [refresh, firstLoad]);
+
+    return (
+        // <TicketList/>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Navigate replace to='/mainBoard'/>}/>
+                    <Route path='/mainBoard' element={<MainBoard counters={counters}/>}/>
+                    <Route path='/counter/:id' element={<CounterScreen/>}/>
+                    <Route path='/customer' element={<CustomerScreen/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- client/src/Components/mainBoard/ShowMainBoard.jsx
+++ client/src/Components/mainBoard/ShowMainBoard.jsx
@@ -1,5 +1,19 @@
-const MainBoard = () => {
+import '../../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
+const MainBoard = (props) => {
+    return(
+        <div className="counter-list">
+            {props.counters.map((counter) => (
+                <div className="counter m-5">
+                        <h1 className="m-5 display-2 fw-bold">COUNTER {counter.counterID}</h1>
+                        <h1 className="m-5">now serving:</h1>
+                        <h1 className="m-5">{counter.nextCustomer}  {counter.service}</h1>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default MainBoard;
