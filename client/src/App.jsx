1405cb9b9391278857dc94eaeac9b5e51b3a4965
filippobertolotti0 <filippobertolotti0@gmail.com>
2023-10-26 19:16:12 +0200

    fixed mainboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,40 +8,6 @@ import CustomerScreen from './Components/customer/customerScreen'
 import axios from "axios";
 
 function App() {
-    const [counters, setCounters] = useState([]);
-    const [firstLoad, setFirstLoad] = useState(false);
-    const [refresh, setRefresh] = useState(false);
-    let list = [];
-
-    useEffect(()=>{
-        if(!firstLoad){
-            setFirstLoad(!firstLoad);
-        }
-        else{
-            axios.get("http://localhost:8080/API/getcounters")
-                .then(async result => {
-                    for (const element of result.data) {
-                        await axios.get(`http://localhost:8080/API/ticket/counter/${element.id}`)
-                            .then(result => {
-                                list.push({counterID: element.id, nextCustomer: result.data.id, service: result.data.serviceTypeId})
-                                console.log(result.data);
-                            })
-                            .catch(err => {
-                                console.log(err);
-                            });
-                    }
-                })
-                .then(() => {
-                    setCounters(list);
-                    console.log("prova app");
-                    console.log("app"+counters);
-                    list = [];
-                })
-                .catch(err => {
-                    console.log('error in catch', err);
-                });
-        }
-    }, [refresh, firstLoad]);
 
     return (
         // <TicketList/>
@@ -49,8 +15,8 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Navigate replace to='/mainBoard'/>}/>
-                    <Route path='/mainBoard' element={<MainBoard counters={counters}/>}/>
-                    <Route path='/counter/:id' element={<CounterScreen counters={counters} refresh={refresh} setRefresh={setRefresh}/>}/>
+                    <Route path='/mainBoard' element={<MainBoard/>}/>
+                    <Route path='/counter/:id' element={<CounterScreen/>}/>
                     <Route path='/customer' element={<CustomerScreen/>}/>
                 </Routes>
             </BrowserRouter>
--- client/src/Components/mainBoard/ShowMainBoard.jsx
+++ client/src/Components/mainBoard/ShowMainBoard.jsx
@@ -1,19 +1,68 @@
-import '../../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import '../../App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const MainBoard = (props) => {
-    return(
-        <div className="counter-list">
-            {props.counters.map((counter) => (
-                <div className="counter m-5">
-                        <h1 className="m-5 display-2 fw-bold">COUNTER {counter.counterID}</h1>
-                        <h1 className="m-5">now serving:</h1>
-                        <h1 className="m-5">{counter.nextCustomer}  {counter.service}</h1>
+    const [tickets, setTickets] = useState([]);
+    const test = [];
+
+    const fetchTickets = () => {
+        axios.get('http://localhost:8080/API/gettickets')
+            .then((response) => {
+                setTickets(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    useEffect(() => {
+        // Imposta il polling per fare una richiesta ogni secondo
+        const pollingInterval = 1000; // 1000 ms = 1 secondo
+        const pollingTimer = setInterval(fetchTickets, pollingInterval);
+
+        // Chiudi il timer quando il componente viene smontato
+        return () => {
+            clearInterval(pollingTimer);
+        };
+
+        // Inizialmente, esegui la prima richiesta
+        fetchTickets();
+    }, []); // Assicurati di passare un array vuoto come secondo argomento per eseguire useEffect solo una volta all'avvio del componente.
+
+    const showTickets = ((tickets) => {
+        if(tickets.length === 0) {
+            return (<></>)
+        }
+        else {
+            const newTicket = tickets[0];
+            tickets.shift();
+            return(
+                <>
+                <div className="main-box">
+                    <p className="lastTicket" style={{fontSize:"80px", fontWeight:"bold"}}>NOW SERVING:</p>
+                    <p className="lastTicket" style={{fontSize:"400px", fontWeight:"bold"}}>{newTicket.number}</p>
+                    <p className="lastTicket" style={{fontSize:"80px", fontWeight:"bold"}}>AT COUNTER {newTicket.counterId}</p>
                 </div>
-            ))}
+                <div className="sidebar">
+                    <p style={{fontSize:"20px", fontWeight:"bold"}}>LAST FIVE TICKET CALLED:</p>
+                    {tickets.map(ticket => {
+                        return(
+                            <p style={{fontSize:"70px", fontWeight:"bold"}}>{ticket.number}</p>
+                        )
+                    })}
+                </div>
+                </>
+            )}
+    })
+
+    return (
+        <body>
+        <div className="container">
+            {showTickets(tickets)}
         </div>
-    )
-}
+        </body>
+    );
+};
 
 export default MainBoard;
