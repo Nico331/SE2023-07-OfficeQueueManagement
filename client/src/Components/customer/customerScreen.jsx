5e03863c4ead1bd1e3700db9d1167c49c3327bff
Soheil Jamshidi <eris1376@gmail.com>
2023-10-26 17:33:06 +0200

    Change customer page visual

--- client/src/Components/customer/customerScreen.jsx
+++ client/src/Components/customer/customerScreen.jsx
@@ -7,7 +7,7 @@ const CustomerScreen = () => {
     const [services, setServices] = useState([]);
     const [sservice, setSservice] = useState("");
     const [nticket, setNticket] = useState('');
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState(false);
 
     useEffect(() => {
         axios.get('http://localhost:8080/API/getservicetypes')
@@ -18,25 +18,9 @@ const CustomerScreen = () => {
                 console.error('Errore nella richiesta API:', error);
             });
     }, []);
-
-    /*const newticket = (event) => {
-        event.preventDefault();
-        axios.post('http://localhost:8080/API/ticket/servicetype', {serviceType: sservice})
-            .then((response) => {
-               setNewticket(response);
-            })
-            .catch((error) => {
-                console.error('Error of post:', error);
-            });
-        console.log(sservice);
-        console.log(newticket);
-    };*/
-
     const newticket = (event) => {
-        // if (sservice !== "") {
-
-
-            event.preventDefault();
+        event.preventDefault();
+        if (sservice !== "") {
             axios.post('http://localhost:8080/API/ticket/servicetype/'+sservice)
                 .then((response) => {
                     setNticket(response.data);
@@ -44,49 +28,51 @@ const CustomerScreen = () => {
                 .catch((error) => {
                     console.error('Error of post:', error);
                 });
-/*        }
+            setMessage(false);
+        }
         else {
-            setMessage("Please select a service");
+            setMessage(true);
             return;
-        }*/
+        }
     };
 
     return (
         <>
-            <Container >
-                <Row>
-                    <Col>
-                        <Navbar>
-
-                        </Navbar>
-                    </Col>
-                </Row>
-                <Row>
+            <Container fluid>
+                <Row className={"justify-content-md-center"} id={"mainform_customer"} style={{marginTop: '1em'}}>
                     <Col>
                         <Form onSubmit={newticket}>
                             <Form.Label>Please select a service:</Form.Label>
-                            <Form.Select defaultValue={""} style={{width: '30%'}} onChange={text => setSservice(text.target.value)}>
+                            <Form.Select style={{marginBottom: '0.5em'}} defaultValue={""} onChange={text => setSservice(text.target.value)}>
                                 <option disabled={true} value="">Select service</option>
                                 {services.map((index) => (
                                     <option key={index.id} value={index.id}>{index.tag}</option>
                                     )
                                 )}
                             </Form.Select>
-                            {/*{message && <Row> <Alert variant={"danger"} show={true} dismissible>{message}</Alert> </Row>}*/}
-                            <Button style={{margin: '1em'}} type="submit">
+                            {message && <Row> <Alert style={{marginLeft: '0.80em', marginTop: '0.5em'}} variant={"danger"} show={true} onClose={() => setMessage(false)} dismissible>Please select a service</Alert> </Row>}
+                            <Button style={{}} type="submit">
                                 New Ticket
                             </Button>
                         </Form>
-                        {nticket === "" ?
-                            null
-                        :
-                            <>
-                                <Form.Label>Ticket id:{nticket.id} </Form.Label>
-                                <br/>
-                                <Form.Label>Waiting time:{nticket.waiting_time}</Form.Label>
-                            </>
-                        }
                     </Col>
+                </Row>
+                <Row style={{marginTop: '0.5em'}} id={"newticket"}>
+                    {nticket === "" ?
+                        null
+                        :
+                        <>
+                            <Col style={{border: 'solid 1px black'}}>
+                                <Form.Label>Ticket id: {nticket.id} </Form.Label>
+                                <br/>
+                                {/*<Form.Label>Type of Service: {services.find((index) => {
+
+                                })}</Form.Label>*/}
+                                
+                                <Form.Label>Waiting time: {nticket.waitingTime}</Form.Label>
+                            </Col>
+                        </>
+                    }
                 </Row>
             </Container>
         </>
