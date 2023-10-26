6a9845e3f88cc62b69fbe58125a2fcc642ac7d03
filippobertolotti0 <filippobertolotti0@gmail.com>
2023-10-26 22:43:27 +0200

    fixed mainboard

--- client/src/Components/mainBoard/ShowMainBoard.jsx
+++ client/src/Components/mainBoard/ShowMainBoard.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const MainBoard = (props) => {
     const [tickets, setTickets] = useState([]);
+    const [services, setServices] = useState([]);
     const test = [];
 
     const fetchTickets = () => {
@@ -30,25 +31,37 @@ const MainBoard = (props) => {
         fetchTickets();
     }, []); // Assicurati di passare un array vuoto come secondo argomento per eseguire useEffect solo una volta all'avvio del componente.
 
+    useEffect(() => {
+        axios.get('http://localhost:8080/API/getservicetypes')
+            .then((response) => {
+                setServices(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     const showTickets = ((tickets) => {
         if(tickets.length === 0) {
             return (<></>)
         }
         else {
             const newTicket = tickets[0];
+            const newTicketService = services.find((service) => service.id === newTicket.serviceType );
             tickets.shift();
             return(
                 <>
                 <div className="main-box">
                     <p className="lastTicket" style={{fontSize:"80px", fontWeight:"bold"}}>NOW SERVING:</p>
-                    <p className="lastTicket" style={{fontSize:"400px", fontWeight:"bold"}}>{newTicket.number}</p>
+                    <p className="lastTicket" style={{fontSize:"400px", fontWeight:"bold"}}>{newTicketService.code}{newTicket.number}</p>
                     <p className="lastTicket" style={{fontSize:"80px", fontWeight:"bold"}}>AT COUNTER {newTicket.counterId}</p>
                 </div>
                 <div className="sidebar">
                     <p style={{fontSize:"20px", fontWeight:"bold"}}>LAST FIVE TICKET CALLED:</p>
                     {tickets.map(ticket => {
+                        let S = services.find((service) => service.id === ticket.serviceType );
                         return(
-                            <p style={{fontSize:"70px", fontWeight:"bold"}}>{ticket.number}</p>
+                            <p style={{fontSize:"70px", fontWeight:"bold"}}>{S}{ticket.number}</p>
                         )
                     })}
                 </div>
